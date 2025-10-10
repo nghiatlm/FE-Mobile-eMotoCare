ddabca8671a628b645dfe38c6ebfb4a7057c60de
nghiatlm <nghiatlmqe170244@fpt.edu.vn>
2025-10-10 23:22:56 +0700

    add InputComponent

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 import SpaceComponent from "./containers/SpaceComponent";
 import ButtonComponent from "./ui/ButtonComponent";
+import InputComponent from "./ui/InputComponent";
 import TextComponent from "./ui/TextComponent";
 
-export { SpaceComponent, TextComponent, ButtonComponent };
+export { SpaceComponent, TextComponent, ButtonComponent, InputComponent };
--- src/screens/auth/LoginScreen.tsx
+++ src/screens/auth/LoginScreen.tsx
@@ -1,20 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { Text, View } from "react-native";
-import { ButtonComponent } from "../../components";
+import { ButtonComponent, InputComponent } from "../../components";
 import { globalStyle } from "../../styles/globalStyle";
+import { Lock, Sms } from "iconsax-react-nativejs";
+import { appColor } from "../../constants/appColor";
 
 const LoginScreen = () => {
+  const [phone, setPhone] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
-    <View style={[globalStyle.container, { padding: 16 }]}>
-      <Text>LoginScreen</Text>
-      <ButtonComponent
-        text="Đăng nhập"
-        onPress={() => console.log("login")}
-        icon={
-          <View>
-            <Text>N</Text>
-          </View>
-        }
+    <View
+      style={[
+        globalStyle.container,
+        {
+          justifyContent: "center",
+          alignItems: "center",
+          padding: 20,
+        },
+      ]}
+    >
+      <InputComponent
+        value={phone}
+        onChange={(val) => setPhone(val)}
+        placeholder="Số điện thoại"
+        allowClear
+        affix={<Sms size={22} color={appColor.gray} />}
+      />
+
+      <InputComponent
+        value={password}
+        onChange={(val) => setPassword(val)}
+        placeholder="Mật khẩu"
+        allowClear
+        isPassword
+        affix={<Lock size={22} color={appColor.gray} />}
       />
     </View>
   );
